b8bda42addc317d06e3fc52a781ece66d01803e7
Paul <paulantonyjose@gmail.com>
2020-05-11 14:20:51 +0530

    a commit

--- showbylike/settings.py
+++ showbylike/settings.py
@@ -39,19 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'showbylikeapp',
-    'debug_toolbar',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-      'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-  
+
 ]
 
 ROOT_URLCONF = 'showbylike.urls'
