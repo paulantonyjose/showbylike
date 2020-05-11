792e671bdf7544c4d3a744ad809b6f1b919bde8a
Paul <paulantonyjose@gmail.com>
2020-05-11 14:29:40 +0530

    a commit

--- showbylike/settings.py
+++ showbylike/settings.py
@@ -138,3 +138,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5
 }
+
+
+import django_heroku
+django_heroku.settings(locals())
